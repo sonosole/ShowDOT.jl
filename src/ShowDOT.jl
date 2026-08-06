aab93974964e5fc1931a53f503ba260c09bbd681
Sole <30993566+sonosole@users.noreply.github.com>
2026-08-06 15:54:03 +0800

    init

---
+++ src/ShowDOT.jl
@@ -0,0 +1,11 @@
+"""
+    Show directed graph via dot formatted string. Here the `Show`DOT means
+    showing it inside a terminal, or save it as a picture so as to show it
+    inside other apps that can open it.
+"""
+module ShowDOT
+
+
+
+end # module ShowDOT
+
